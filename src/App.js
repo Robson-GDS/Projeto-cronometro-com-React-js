06e43a5452d77c47f91175ca164aa72709ffe6ea
Robson-GDS <77689659+Robson-GDS@users.noreply.github.com>
2021-02-10 09:16:27 -0300

    codigo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            numero: 0,
+            botao: 'VAI'
+        };
+        this.timer = null;
+        this.vai = this.vai.bind(this);
+        this.limpar = this.limpar.bind(this);
+    }
+
+    vai(){
+        let state = this.state;
+
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+            state.botao = 'VAI'
+        }else{
+            this.timer = setInterval(() => {
+                let state = this.state;
+                state.numero += 0.1;
+                this.setState(state);
+            }, 100);
+            state.botao = 'PAUSAR';
+        }
+        
+        this.setState(state);
+    }
+
+
+    limpar(){
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer = null;
+        }
+
+        let state = this.state;
+        state.numero = 0;
+        state.botao = 'VAI';
+        this.setState(state);
+    }
+
+    render(){
+        return(
+            <div className='container'>
+                <a className='timer'>{this.state.numero.toFixed(1)}</a>
+                <div className='areaBtn'>
+                    <a className='botao' onClick={this.vai}>{this.state.botao}</a>
+                    <a className='botao' onClick={this.limpar}>Limpar</a>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
